497154b406fedc62ad62515fa0af02451e6b2ccd
Marek <74934496+Only-Czesio@users.noreply.github.com>
2023-07-11 18:46:22 +0200

    Add files via upload

--- logic.js
+++ logic.js
@@ -1,65 +1,95 @@
 const blackCoin = "blackCoin";
 const whiteCoin = "whiteCoin";
-const cellElements = document.querySelectorAll("[data-square]");
-const boardRowsColumns = [
-  [0, 1, 2, 3, 4, 5, 6, 7],
-  [8, 9, 10, 11, 12, 13, 14, 15],
-  [16, 17, 18, 19, 20, 21, 22, 23],
-  [24, 25, 26, 27, 28, 29, 30, 31],
-  [32, 33, 34, 35, 36, 37, 38, 39],
-  [40, 41, 42, 43, 44, 45, 46, 47],
-  [48, 49, 50, 51, 52, 53, 54, 55],
-  [56, 57, 58, 59, 60, 61, 62, 63],
-];
-const edgeOfaBoard = [
-  0, 1, 2, 3, 4, 5, 6, 7, 8, 15, 16, 23, 24, 31, 32, 39, 40, 47, 48, 55, 56, 57,
-  58, 59, 60, 61, 62, 63,
+const cells = document.querySelectorAll("[data-square]");
+const board = document.getElementById("board");
+ 
+// Define the directions to check for neighbors
+const directions = [
+  { row: -1, column: -1 }, // top left
+  { row: -1, column: 0 }, // top
+  { row: -1, column: 1 }, // top right
+  { row: 0, column: -1 }, // left
+  { row: 0, column: 1 }, // right
+  { row: 1, column: -1 }, // bottom left
+  { row: 1, column: 0 }, // bottom
+  { row: 1, column: 1 }, // bottom right
 ];
 
 let whoseTurn;
 let scoreB = 0;
 let scoreW = 0;
 
-startGame();
+let matrix = [];
+
+function listToMatrix(list, size) {
+  let i, k;
+  for (i = 0, k = -1; i < list.length; i++) {
+    if (i % size === 0) {
+      k++;
+      matrix[k] = [];
+    }
+    matrix[k].push(list[i]);
+  }
+  return matrix;
+}
+
+
+
+console.log(listToMatrix(cells, 8))
 
 function startGame() {
-    whoseTurn = false;
-        cellElements[27].classList.add("whiteCoin");
-        cellElements[28].classList.add("blackCoin");
-        cellElements[35].classList.add("blackCoin");
-        cellElements[36].classList.add("whiteCoin");
-  cellElements.forEach((cell) => {
+  listToMatrix(cells, 8);
+  whoseTurn = false;
+  matrix[3][3].classList.add("whiteCoin");
+  matrix[3][4].classList.add("blackCoin");
+  matrix[4][3].classList.add("blackCoin");
+  matrix[4][4].classList.add("whiteCoin");
+  cells.forEach((cell) => {
     cell.addEventListener("click", handleClick, { once: true });
   });
-//   setBoardHoverClass();
+
+    setBoardHoverClass();
 }
 
 function handleClick(clicked) {
   const cell = clicked.target;
-    cellElements[27].removeEventListener("click", handleClick);
-    cellElements[28].removeEventListener("click", handleClick);
-    cellElements[35].removeEventListener("click", handleClick);
-    cellElements[36].removeEventListener("click", handleClick);
-    const currentClass = whoseTurn ? blackCoin : whiteCoin;
-    let index = getIndex(cell);
-    console.log(hasNeighbor(index));
-    if (hasNeighbor(index)) {
-        placeMark(cell, currentClass);
-        swapTurns();
-    }
-//   setBoardHoverClass();
-
+    matrix[3][3].removeEventListener("click", handleClick);
+    matrix[3][4].removeEventListener("click", handleClick);
+    matrix[4][3].removeEventListener("click", handleClick);
+    matrix[4][4].removeEventListener("click", handleClick);
+  cell.removeEventListener("click", handleClick);
+  const currentClass = whoseTurn ? blackCoin : whiteCoin;
+  let index = getIndex(cell);
+  console.log(hasNeighbor(index));
+  if (hasNeighbor(index) && captureOpponentCoins(getIndex(cell),currentClass)) {
+    placeMark(cell, currentClass);
+    swapTurns();
+  }
+  if (findLastNeighborClass(cell, currentClass) === currentClass) {
+    captureOpponentCoins(cell, currentClass);
+  }
+    setBoardHoverClass();
+}
+function setBoardHoverClass() {
+  board.classList.remove(X_CLASS);
+  board.classList.remove(CIRCLE_CLASS);
+  if(whoseTurn) {
+    board.classList.add(blackCoin);
+  } else {
+    board.classList.add(whiteCoin);
+  }
 }
 
 function placeMark(cell, currentClass) {
   cell.classList.add(currentClass);
+  captureOpponentCoins(getIndex(cell), currentClass);
 }
 
 function swapTurns() {
   whoseTurn = !whoseTurn;
 }
 function getIndex(clicked) {
-  let idx = [].indexOf.call(cellElements, clicked);
+  let idx = [].indexOf.call(cells, clicked);
   console.log(idx);
   return idx;
 }
@@ -67,31 +97,19 @@ function hasNeighbor(cellIndex) {
   const cellRow = Math.floor(cellIndex / 8);
   const cellColumn = cellIndex % 8;
 
-  // Define the directions to check for neighbors
-  const directions = [
-    { row: -1, column: -1 }, // top left
-    { row: -1, column: 0 }, // top
-    { row: -1, column: 1 }, // top right
-    { row: 0, column: -1 }, // left
-    { row: 0, column: 1 }, // right
-    { row: 1, column: -1 }, // bottom left
-    { row: 1, column: 0 }, // bottom
-    { row: 1, column: 1 }, // bottom right
-  ];
-
-  for(const direction of directions) {
+  for (const direction of directions) {
     const neighborRow = cellRow + direction.row;
     const neighborColumn = cellColumn + direction.column;
     const neighborIndex = neighborRow * 8 + neighborColumn;
 
     // Check if the neighbor is within the board boundaries
-    if(
+    if (
       neighborRow >= 0 &&
       neighborRow < 8 &&
       neighborColumn >= 0 &&
       neighborColumn < 8
     ) {
-      const neighborCell = cellElements[neighborIndex];
+      const neighborCell = cells[neighborIndex];
       // Check if the neighbor cell is occupied by a coin
       if (
         neighborCell.classList.contains(blackCoin) ||
@@ -103,7 +121,7 @@ function hasNeighbor(cellIndex) {
   }
   return false;
 }
-function findLastNeighbor(
+function findLastNeighborClass(
   cellIndex,
   currentClass,
   directionRow,
@@ -123,7 +141,7 @@ function findLastNeighbor(
     neighborColumn >= 0 &&
     neighborColumn < 8
   ) {
-    const neighborCell = cellElements[neighborIndex];
+    const neighborCell = cells[neighborIndex];
 
     // Check if the neighbor cell is occupied by a coin of the opposite color
     if (
@@ -131,7 +149,7 @@ function findLastNeighbor(
         currentClass === blackCoin ? whiteCoin : blackCoin
       )
     ) {
-      return findLastNeighbor(
+      return findLastNeighborClass(
         neighborIndex,
         currentClass,
         directionRow,
@@ -140,5 +158,69 @@ function findLastNeighbor(
     }
   }
 
-  return cellIndex;
+  return currentClass;
 }
+function captureOpponentCoins(cellIndex, currentClass) {
+
+  let canCapture = false;
+
+  const cellRow = Math.floor(cellIndex / 8);
+  const cellColumn = cellIndex % 8;
+
+  for (const direction of directions) {
+    let neighborRow = cellRow + direction.row;
+    let neighborColumn = cellColumn + direction.column;
+    let neighborIndex = neighborRow * 8 + neighborColumn;
+
+    // Check if the neighbor is within the board boundaries
+    if (
+      neighborRow >= 0 &&
+      neighborRow < 8 &&
+      neighborColumn >= 0 &&
+      neighborColumn < 8
+    ) {
+      let neighborCell = cells[neighborIndex];
+
+      // Check if the neighbor cell is occupied by the opponent's coin
+      if (
+        neighborCell.classList.contains(
+          currentClass === blackCoin ? whiteCoin : blackCoin
+        )
+      ) {
+        let capturedCoins = [];
+
+        // Continue in the same direction until reaching a cell that is not occupied by the opponent's coin
+        while (
+          neighborRow >= 0 &&
+          neighborRow < 8 &&
+          neighborColumn >= 0 &&
+          neighborColumn < 8 &&
+          neighborCell.classList.contains(
+            currentClass === blackCoin ? whiteCoin : blackCoin
+          )
+        ) {
+          capturedCoins.push(neighborCell);
+          neighborRow += direction.row;
+          neighborColumn += direction.column;
+          neighborIndex = neighborRow * 8 + neighborColumn;
+          neighborCell = cells[neighborIndex];
+        }
+
+        // Check if the last cell in the direction is occupied by the current player's coin
+        if (neighborCell?.classList.contains(currentClass)) {
+          // Capture the opponent's coins by removing their class and adding the current player's class
+          capturedCoins.forEach((capturedCoin) => {
+            capturedCoin.classList.remove(
+              currentClass === blackCoin ? whiteCoin : blackCoin
+            );
+            capturedCoin.classList.add(currentClass);
+            canCapture = true;
+          });
+        }
+      }
+    }
+  }
+  return canCapture;
+}
+
+startGame();
